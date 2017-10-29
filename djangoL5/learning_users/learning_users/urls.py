c6ad6e56f25eceb643c98d0ea9c864f1b44888ff
williamdelrosario <williamdelrosario@yahoo.com>
2017-10-29 09:01:30 +0100

    Registration

--- djangoL5/learning_users/learning_users/urls.py
+++ djangoL5/learning_users/learning_users/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from basic_app import views
 
 urlpatterns = [
     url(r'^$', views.index,name='index'),
